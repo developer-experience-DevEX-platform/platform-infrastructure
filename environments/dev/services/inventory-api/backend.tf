f0d2bc432870837155ec4925a2d4345837029055
temi <adebunmi33@gmail.com>
2026-08-31 22:27:32 +0100

    Remove obsolete service identity definitions

--- environments/dev/services/inventory-api/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Backstage-workflow"
-    storage_account_name = "devexplatformtfstate"
-    container_name       = "tfstate"
-    key                  = "dev/services/inventory-api.tfstate"
-    use_azuread_auth     = true
-  }
-}
