24372028c283843bbbc0451d1d2b4f6f3c81f246
temi <adebunmi33@gmail.com>
2026-09-02 12:03:22 +0100

    fix: allow EKS nodegroup service role inspection

--- bootstrap/aws-account/main.tf
+++ bootstrap/aws-account/main.tf
@@ -228,6 +228,18 @@ data "aws_iam_policy_document" "terraform_platform" {
   }
 
   statement {
+    sid = "ReadEKSNodegroupServiceLinkedRole"
+
+    actions = [
+      "iam:GetRole",
+    ]
+
+    resources = [
+      "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup",
+    ]
+  }
+
+  statement {
     sid = "ReadNetworkInfrastructure"
 
     actions = [
